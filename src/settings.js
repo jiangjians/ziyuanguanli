29d2391824fa5da0fdca167503084533e076cff3
jiangjians <1455928402@qq.com>
2022-07-13 11:50:22 +0800

    '项目初始化'

---
+++ src/settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+  title: '人力资源',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   * 设置appMain右侧主体上面的header是不是固定定位的
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   * 控制侧边栏logo的显示
+   */
+  sidebarLogo: true
+}
